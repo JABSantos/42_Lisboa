30bc3cc7be1a662122383f2a3d608221d861df39
JABSantos <78555896+JABSantos@users.noreply.github.com>
2021-02-19 21:26:01 +0000

    19/02

---
+++ ft_substr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josantos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/19 19:43:52 by josantos          #+#    #+#             */
+/*   Updated: 2021/02/19 21:10:03 by josantos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	size_t a;
+	char *p;
+
+	a = 0;
+	if (s == NULL)
+		return (0);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len >= ft_strlen(s))
+	{
+		if(!(p = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1))))
+			return(0);
+	}
+	else if (!(p = (char *)malloc(sizeof(char) * len + 1)))
+		return (0);
+	while (s[a] && a < len)
+	{
+		p[a++] = s[start++];
+	}
+	p[a] = '\0';
+	return(p);
+}
+
---
+++ teste_mains.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   teste_mains.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josantos <josantos@42lisboa.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/08 19:21:24 by josantos          #+#    #+#             */
+/*   Updated: 2021/02/19 17:27:52 by josantos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int		main(void)
+{	
+	//char *t;
+	//char s[] = "";
+	//char b[] = "";
+
+	printf("%d\n", strncmp("345", "845", 3));
+	//printf("%s\n", t);
+	printf("%d\n", ft_strncmp("345", "845", 1));
+		//printf("%s\n", ft_memmove(sResult + 1, sResult2, 2));
+}
